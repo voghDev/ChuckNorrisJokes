02e2ec3e4b3010dcc92be768b93b2e1d6dd8f138
Olmo Gallegos <vendofiatpunto@hotmail.com>
2017-10-29 19:51:43 +0100

    Remove whitespace

--- app/src/main/kotlin/es/voghdev/chucknorrisjokes/ui/fragment/JokeByKeywordFragment.kt
+++ app/src/main/kotlin/es/voghdev/chucknorrisjokes/ui/fragment/JokeByKeywordFragment.kt
@@ -11,7 +11,6 @@ class JokeByKeywordFragment : BaseFragment(), JokeByKeywordPresenter.MVPView, Jo
 
     var presenter: JokeByKeywordPresenter? = null
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
