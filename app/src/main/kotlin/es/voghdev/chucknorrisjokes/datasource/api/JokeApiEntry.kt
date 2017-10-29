c0d5aa3b2746e30a5cd7e5a2b76a2fe9a91a56ac
Olmo Gallegos <vendofiatpunto@hotmail.com>
2017-10-29 20:01:45 +0100

    ApiImpl and mapper function

--- app/src/main/kotlin/es/voghdev/chucknorrisjokes/datasource/api/JokeApiEntry.kt
+++ app/src/main/kotlin/es/voghdev/chucknorrisjokes/datasource/api/JokeApiEntry.kt
@@ -1,9 +1,19 @@
 package es.voghdev.chucknorrisjokes.datasource.api
 
+import es.voghdev.chucknorrisjokes.model.Joke
+
 class JokeApiEntry(
         val id: String = "",
         val icon_url: String = "",
         val url: String = "",
         val value: String = "",
         val category: List<String>? = emptyList()
-)
+){
+    inline fun map(): Joke = Joke(
+            id = id,
+            iconUrl = icon_url,
+            url = url,
+            value = value,
+            categories = category ?: emptyList()
+    )
+}
