d29785ccf5ccdb18f77d55dbab6280e97d1b184b
Olmo Gallegos <vendofiatpunto@hotmail.com>
2017-10-30 23:56:23 +0100

    Add Any matchers

--- app/src/test/kotlin/es/voghdev/chucknorrisjokes/MockitoExtensions.kt
+++ app/src/test/kotlin/es/voghdev/chucknorrisjokes/MockitoExtensions.kt
@@ -2,7 +2,16 @@ package es.voghdev.chucknorrisjokes
 
 import com.nhaarman.mockito_kotlin.whenever
 import es.voghdev.chucknorrisjokes.app.ResLocator
+import org.mockito.Mockito
 
 fun ResLocator.hasString(id: Int, str: String) {
     whenever(getString(id)).thenReturn(str)
+}
+
+fun <T> anyCategory(): T {
+    return Mockito.anyObject<T>()
+}
+
+fun <T> anyJoke(): T {
+    return Mockito.anyObject<T>()
 }
