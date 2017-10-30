6ed567ac57c47987b37aacee6da082d3de686192
Olmo Gallegos <vendofiatpunto@hotmail.com>
2017-10-31 00:01:49 +0100

    Fix service

--- app/src/main/kotlin/es/voghdev/chucknorrisjokes/datasource/api/ChuckNorrisService.kt
+++ app/src/main/kotlin/es/voghdev/chucknorrisjokes/datasource/api/ChuckNorrisService.kt
@@ -3,6 +3,7 @@ package es.voghdev.chucknorrisjokes.datasource.api.model
 import es.voghdev.chucknorrisjokes.datasource.api.JokeApiEntry
 import retrofit2.Call
 import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Query
 
@@ -16,10 +17,10 @@ interface ChuckNorrisService {
     fun getJokeCategories(): Call<List<String>>
 
     @FormUrlEncoded
-    @POST("jokes/search")
+    @GET("jokes/search")
     fun getRandomJokeByKeyword(@Query("query") keyword: String): Call<JokeByKeywordApiResponse>
 
     @FormUrlEncoded
-    @POST("jokes/random")
+    @GET("jokes/random")
     fun getRandomJokeByCategory(@Query("category") category: String): Call<JokeApiEntry>
 }
