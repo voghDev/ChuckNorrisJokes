bf5bf01d4cb19ff671ca17361e7a2bc840019d8d
Olmo Gallegos <vendofiatpunto@hotmail.com>
2017-11-01 21:12:19 +0100

    Rename usecase, implement GetRandomJokeByKeywordApiImpl

--- app/src/main/kotlin/es/voghdev/chucknorrisjokes/usecase/GetRandomJokeByKeyword.kt
+++ app/src/main/kotlin/es/voghdev/chucknorrisjokes/usecase/GetRandomJokeByKeyword.kt
@@ -3,6 +3,6 @@ package es.voghdev.chucknorrisjokes.usecase
 import es.voghdev.chucknorrisjokes.model.AbsError
 import es.voghdev.chucknorrisjokes.model.Joke
 
-interface SearchJokesByKeyword {
-    fun searchJokesByKeyword(keyword: String): Pair<List<Joke>?, AbsError?>
+interface GetRandomJokeByKeyword {
+    fun getRandomJokeByKeyword(keyword: String): Pair<List<Joke>?, AbsError?>
 }
