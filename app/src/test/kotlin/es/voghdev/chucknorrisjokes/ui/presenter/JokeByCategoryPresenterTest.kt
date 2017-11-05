6c308b35d5e3d66eda3453e3970ec1ff7eba396f
Olmo Gallegos <vendofiatpunto@hotmail.com>
2017-11-05 19:43:22 +0100

    Add sample code to make workshop easier

--- app/src/test/kotlin/es/voghdev/chucknorrisjokes/ui/presenter/JokeByCategoryPresenterTest.kt
+++ app/src/test/kotlin/es/voghdev/chucknorrisjokes/ui/presenter/JokeByCategoryPresenterTest.kt
@@ -1,6 +1,9 @@
 package es.voghdev.chucknorrisjokes.ui.presenter
 
+import com.nhaarman.mockito_kotlin.argumentCaptor
 import es.voghdev.chucknorrisjokes.app.ResLocator
+import es.voghdev.chucknorrisjokes.model.Joke
+import es.voghdev.chucknorrisjokes.model.JokeCategory
 import es.voghdev.chucknorrisjokes.repository.ChuckNorrisRepository
 import org.junit.Before
 import org.mockito.Mock
@@ -16,6 +19,19 @@ class JokeByCategoryPresenterTest() {
     lateinit var presenter : JokeByCategoryPresenter
 
     @Mock lateinit var mockChuckNorrisRepository : ChuckNorrisRepository
+
+    val categories = listOf(
+            JokeCategory("Politics"),
+            JokeCategory("Sports")
+    )
+
+    val exampleJoke = Joke(id = "abc",
+            iconUrl = "http://chuck.image.url",
+            url = "http://example.url",
+            value = "We have our fears, fear has its Chuck Norris'es")
+
+    val categoryCaptor = argumentCaptor<JokeCategory>()
+    val strCaptor = argumentCaptor<String>()
 
     @Before
     fun setUp() {
