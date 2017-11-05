9cebe02b6e2cdc8130a496c7c5b94f6d9005c60a
Olmo Gallegos <vendofiatpunto@hotmail.com>
2017-11-05 21:30:13 +0100

    Solve two limitations during implementation

--- app/src/main/kotlin/es/voghdev/chucknorrisjokes/ui/fragment/JokeByCategoryFragment.kt
+++ app/src/main/kotlin/es/voghdev/chucknorrisjokes/ui/fragment/JokeByCategoryFragment.kt
@@ -8,6 +8,7 @@ import es.voghdev.chucknorrisjokes.datasource.api.GetJokeCategoriesApiImpl
 import es.voghdev.chucknorrisjokes.datasource.api.GetRandomJokeApiImpl
 import es.voghdev.chucknorrisjokes.datasource.api.GetRandomJokeByCategoryApiImpl
 import es.voghdev.chucknorrisjokes.datasource.api.GetRandomJokeByKeywordApiImpl
+import es.voghdev.chucknorrisjokes.model.JokeCategory
 import es.voghdev.chucknorrisjokes.repository.ChuckNorrisRepository
 import es.voghdev.chucknorrisjokes.ui.presenter.JokeByCategoryPresenter
 import kotlinx.coroutines.experimental.runBlocking
@@ -32,7 +33,18 @@ class JokeByCategoryFragment : BaseFragment(), JokeByCategoryPresenter.MVPView, 
         runBlocking {
             presenter?.initialize()
         }
+
+//        btn_search.setOnClickListener {
+//            runBlocking {
+//                presenter?.onSearchButtonClicked(spn_categories.selectedItemPosition)
+//            }
+//        }
     }
+
+//    override fun fillCategoriesSpinner(categories: List<JokeCategory>) {
+//        val names = categories.map { c -> c.name }
+//        spn_categories.configureDefaultAdapter(names)
+//    }
 
     override fun getLayoutId(): Int {
         return R.layout.fragment_joke_by_category
