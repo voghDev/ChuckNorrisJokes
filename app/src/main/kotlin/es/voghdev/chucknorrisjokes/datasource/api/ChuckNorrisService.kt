abc729e60109408e4542a1451af4a1f89ffddaf1
Olmo Gallegos <vendofiatpunto@hotmail.com>
2018-01-12 19:30:11 +0100

    Reformat code, add apache2 headers

--- app/src/main/kotlin/es/voghdev/chucknorrisjokes/datasource/api/ChuckNorrisService.kt
+++ app/src/main/kotlin/es/voghdev/chucknorrisjokes/datasource/api/ChuckNorrisService.kt
@@ -2,9 +2,7 @@ package es.voghdev.chucknorrisjokes.datasource.api.model
 
 import es.voghdev.chucknorrisjokes.datasource.api.JokeApiEntry
 import retrofit2.Call
-import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
-import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface ChuckNorrisService {
